75d8d59475fcba8d7bcd36a487a772a53d56dc6d
Kakashiiiiy <benedict.schlueter@rub.de>
2019-12-02 18:16:37 +0100

    Standardabweichungsrechner fertig

--- Abgabe_3/Data/s_abw.py
+++ Abgabe_3/Data/s_abw.py
@@ -7,27 +7,36 @@ args_parser = ArgumentParser()
 args_parser.add_argument("-F", type=path, required=True, help="Path to measurement")
 
 args, unknown  = args_parser.parse_known_args()
+a = "00124b0001542bf5_00124b0001542c6d_00124b0001542c6d.csv"
+b = "00124b0001542c6d_00124b0001542bf5_00124b0001542bf5.csv"
+c = "00124b00015735cf_00124b0001542bf5_00124b0001542bf5.csv"
+path_ = args.F 
 
-mittelwert = 0 
+def st_abw(path_):
+	mittelwert = 0 
+	with open(path_, 'r') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=' ')
+		for row in spamreader:
+			x = str(row[0].split(";")[1:]).strip("[]'")
+			x = int(str(x),10)
+			mittelwert=(mittelwert+x)
+	
+		mittelwert/=spamreader.line_num	
+	abweichung = 0
+	with open(path_, 'r') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=' ')
+		for row in spamreader:
+			x = str(row[0].split(";")[1:]).strip("[]'")
+			x = int(str(x),10)
+			abweichung+= (x-mittelwert)**2
+		abweichung/=spamreader.line_num
+		abweichung = math.sqrt(abweichung)
 
-with open(args.F, 'r') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ')
-	for row in spamreader:
-		x = str(row[0].split(";")[1:]).strip("[]'")
-		x = int(str(x),10)
-		mittelwert=(mittelwert+x)
+	return abweichung,mittelwert
 
-	mittelwert/=spamreader.line_num
-	print(mittelwert)
-
-abweichung = 0
-with open(args.F, 'r') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ')
-	for row in spamreader:
-		x = str(row[0].split(";")[1:]).strip("[]'")
-		x = int(str(x),10)
-		abweichung+= (x-mittelwert)**2
-	abweichung/=spamreader.line_num
-	abweichung = math.sqrt(abweichung)
-
-print("Der Mittelwert ist :",mittelwert, " bei einer Standardabweichung von ", abweichung)
+abweichung,mittelwert = st_abw(path_+"/"+a)
+print("Alice Mittelwert ist :",mittelwert, " bei einer Standardabweichung von ", abweichung)
+abweichung,mittelwert = st_abw(path_+"/"+b)
+print("Bob's Mittelwert ist :",mittelwert, " bei einer Standardabweichung von ", abweichung)
+abweichung,mittelwert = st_abw(path_+"/"+c)
+print("Eve's Mittelwert ist :",mittelwert, " bei einer Standardabweichung von ", abweichung)
