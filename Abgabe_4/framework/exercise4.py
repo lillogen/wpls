3776697354c75c87c8cdb63ac65cd9d2e6cf8189
Kakashiiiiy <benedict.schlueter@rub.de>
2019-12-21 16:15:19 +0100

    Assignment 4
    Beide Quantisierer implementiert, ba=ab!=ae aber stimmen nicht mit den Testvektoren überein

--- Abgabe_4/framework/exercise4.py
+++ Abgabe_4/framework/exercise4.py
@@ -21,6 +21,7 @@ import utils
 from utils import gray_code
 import numpy
 from subprocess import check_output
+import math
 
 
 """
@@ -35,7 +36,14 @@ Blockwise application is done outside so please use the whole vectors at once.
 
 
 def ber(X, Y):
-    return utils.not_yet_implemented("BER")
+    le = len(X)
+    hw_arg = numpy.bitwise_xor(X,Y)
+    z = numpy.count_nonzero( hw_arg ==1 )
+    ber = float(z)/le
+    if ber > 0.5:
+        return 1 - ber + 1.0/le #Wenn X = inv(Y) ist die Biterrorrate = 1 bit und nicht 0, daher plus 1/le
+    else: 
+        return ber
 
 
 """
@@ -50,7 +58,7 @@ def MI(A, B):
     with open("MI_temp.dat", "w+") as tmp:
         for (a, b) in zip(A, B):
             tmp.write("%f %f\n" % (a, b))
-    #return float(check_output(["./MIhigherdim", "MI_temp.dat", "2", "1", "1", "%d" % len(A), "8"]))
+    return float(check_output(["./MIhigherdim", "MI_temp.dat", "2", "1", "1", "%d" % len(A), "8"]))
 
 
 """
@@ -82,7 +90,6 @@ def quant1(A, B, E, args):
     ### YOUR CODE GOES HERE ###
     max_a = [max(A),max(B),max(E)]
     min_a = [min(A),min(B),min(E)]
-    import math
     ran = [max_a[0]-min_a[0],max_a[1]-min_a[1],max_a[2]-min_a[2]]
     
     ran = [1 if x==0 else x for x in ran]
@@ -90,15 +97,7 @@ def quant1(A, B, E, args):
     #n = [math.log(ran[0],2),math.log(ran[1],2),math.log(ran[2],2)]
     
     m = 2**number_of_bits
-
-    x = [0]*m
-    #print(number_of_bits)
-    #ran = ran[]/m
-    x = gray_code(number_of_bits)
-    x = numpy.flip(x,0)
-    print("gray_code_Array",x)
-    print("Range:", ran)
-
+    x = numpy.flip(gray_code(number_of_bits),0)
     for i in range(0,len(A)):
         gcode = int(math.floor(float(abs(A[i])-abs(max_a[0]))*m/ran[0]))
         if gcode == m:
@@ -115,18 +114,10 @@ def quant1(A, B, E, args):
             gcode= m-1
         bE.append(x[gcode])
 
-    #bA = [m-1 if x==m else x for x in bA]
-    #bB = [m-1 if x==m else x for x in bB]
-    #bE = [m-1 if x==m else x for x in bE]
-
-    #print(bA)
     bA = numpy.concatenate(bA,axis=0).tolist()
     bB = numpy.concatenate(bB,axis=0).tolist()
     bE = numpy.concatenate(bE,axis=0).tolist()
     return (bA,bB,bE)  
-
-    #return utils.not_yet_implemented("quant1")
-    ##After implementation, return binary vectors as tuple
     
 def quant2(A, B, E, args):
     bA = []
@@ -146,8 +137,105 @@ def quant2(A, B, E, args):
     """
 
     ### YOUR CODE GOES HERE ###
+    m_a = numpy.mean(A)
+    m_b = numpy.mean(B)
+    m_e = numpy.mean(E)
 
-    return utils.not_yet_implemented("quant2")
+    std_a = numpy.std(A)
+    std_b = numpy.std(B)
+    std_e = numpy.std(E)
 
-    # return bA, bB, bE  # After implementation, return binary vectors as tuple
+
+    q_a_plus = (m_a + alpha * std_a)
+    q_b_plus = (m_b + alpha * std_b)
+    q_e_plus = (m_e + alpha * std_e)
+
+    q_a_minus = (m_a - alpha * std_a)
+    q_b_minus = (m_b - alpha * std_b)
+    q_e_minus = (m_e - alpha * std_e)
+
+    print(q_b_plus,q_b_minus)
+
+    L = []
+    i = 0
+
+    while i < len(A)-m:
+        if A[i] < q_a_minus:
+            x = 0
+            for z in range(0,m):
+                if A[i+z] >= q_a_minus:
+                    x = 1
+                    break
+            #Wenn es m Werte gab anhängen
+            i_start = i
+            #immer bis zum letzten Element durchgehen
+            while A[i] < q_a_minus and i < len(A)-1:
+                i+=1
+            if x == 1:
+                continue
+            app_var = math.floor(float(i_start+i)/2)
+            L.append(app_var)
+            continue
+
+        if A[i] > q_a_plus:
+            x = 0
+            for z in range(0,m):
+                if A[i+z] <= q_a_plus:
+                    x = 1
+                    break
+            
+            #Wenn es m Werte gab anhängen
+            i_start = i
+            #immer bis zum letzten Element durchgehen
+            while A[i] > q_a_plus and i < len(A)-1:
+                i+=1
+
+            if x == 1:
+                continue
+            app_var = math.floor(float(i_start+i)/2)
+            L.append(app_var)
+            continue
+
+        i+=1
+    #print(L)
+
+    #Berechne Lschlange
+    
+    L_Schlange = []
+    for i in L:
+        x = 0 
+        start = i-math.floor(float(m-2)/2)
+        if B[start] < q_b_minus :
+            for z in range(0,m-1):
+                if B[start+z] >= q_b_minus:
+                    x = 1
+            if x != 1:
+                print("Null")
+                L_Schlange.append(i)
+                bB.append(0)
+            continue
+
+        if B[start] > q_b_plus :
+            for z in range(0,m-1):
+                if B[start+z] <= q_b_minus:
+                    x = 1
+            if x != 1:
+                L_Schlange.append(i)
+                bB.append(1)
+
+    print(L_Schlange)
+
+    for i in L_Schlange:
+        if A[i] > q_a_plus:
+            bA.append(1)
+        elif A[i] < q_a_minus:
+            bA.append(0)
+
+
+        if E[i] > q_e_plus:
+            bE.append(1)
+        elif E[i] < q_e_minus:
+            bE.append(0)
+
+    return bA, bB, bE  # After implementation, return binary vectors as tuple
 
